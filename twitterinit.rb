1b20a8cdb1d84b257e13db372eebbc44d8e8a544
Matthew Donoughe <mdonoughe@gmail.com>
2009-09-25 23:17:58 +0000

    set the URI when running twitterinit.rb
    This makes installation easier.

--- twitterinit.rb
+++ twitterinit.rb
@@ -4,7 +4,8 @@ require 'yaml'
 
 APP_TOKEN = 'HgPXVDITRITul4peTLkyg'
 APP_SECRET = 'iLoBnhZF3YDki5k4l09hptgg9SaRJkOYEyzsk1n0'
-$config = {}
+# set the uri here for convenience during installation
+$config = {:player_list_uri => 'http://20cent.hvzsource.com/players.php'}
 config_path = File::expand_path('~/.wtth.yml')
 if File.exists?(config_path)
   File.open(config_path) do |file|
